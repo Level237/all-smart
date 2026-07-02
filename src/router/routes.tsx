22c1a6f86edaadbe2fe96163d5785e25b31fcc4b
Level237 <bramslevel129@gmail.com>
2026-07-02 14:37:05 +0100

    update

--- src/router/routes.tsx
+++
@@ -1,7 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import Homepage from "../pages/Homepage";
-
-export const routes=createBrowserRouter([{
-    path:'/',
-    element:<Homepage/>
-}])
